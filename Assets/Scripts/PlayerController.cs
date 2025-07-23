caa5673ec2697b78e58eb9917590e71a077c60e0
Kentops <36288496+Kentops@users.noreply.github.com>
2025-07-22 22:32:12 -0700

    Improved border interactions

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -2,33 +2,20 @@ using UnityEngine;
 using UnityEngine.InputSystem;
 public class PlayerController : MonoBehaviour
 {
-
-    private int i;
     public InputAction moveAction;
-
-
-    public Transform playerTransform;
-
 
     public bool playerCanMove;
     public float playerSpeed;
 
+    [SerializeField] private InputActionReference Move;
 
     private Vector3 inputMovement;
-
-
-    [SerializeField]
-    private InputActionReference Move;
 
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-    
-        playerTransform = GetComponent<Transform>();
-        i = 0;
-
-
+        
     }
 
     // Update is called once per frame
@@ -36,26 +23,32 @@ public class PlayerController : MonoBehaviour
     {
         if (playerCanMove) Movement();
     }
+
     void Movement()
     {
         //sets the inputMovement to whatever the player has input
         inputMovement.x = Move.action.ReadValue<Vector2>().x;
         inputMovement.y = Move.action.ReadValue<Vector2>().y;
 
-        i++;
-        if (i > 200) { i = 0;  Debug.Log(""+inputMovement.x); }
-
-
-
-
-
         //moves the player in the direction the player's input at [playerSpeed] units per second
         inputMovement.Normalize();
-        playerTransform.Translate(inputMovement * playerSpeed * Time.deltaTime);
+
+        RaycastHit2D rayCheck = Physics2D.Raycast(transform.position, transform.TransformDirection(inputMovement), 0.33f);
+        if(rayCheck)
+        {
+            if (rayCheck.transform.gameObject.layer == LayerMask.NameToLayer("Horizontal Wall"))
+            {
+                inputMovement.y = 0;
+            }
+            if (rayCheck.transform.gameObject.layer == LayerMask.NameToLayer("Vertical Wall"))
+            {
+                inputMovement.x = 0;
+            }
+            inputMovement.Normalize();
+        }
 
 
-
-
+            transform.Translate(inputMovement * playerSpeed * Time.deltaTime);
     }
 
 }
