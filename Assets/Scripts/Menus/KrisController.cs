48e2510ca95c7150b03b37d2dd58f31d1093f6f7
Kentops <36288496+Kentops@users.noreply.github.com>
2025-07-26 14:43:14 -0700

    Added Kris
    Added and somewhat animated Kris

---
+++ Assets/Scripts/Menus/KrisController.cs
@@ -0,0 +1,29 @@
+using UnityEngine;
+
+public class KrisController : MonoBehaviour
+{
+    //0 = Start, 1 = idle, 2 = fight, 3 = act/spare, 4 = item, 5 = defend, 6 = hurt, 7 = defeated, 8 = exit
+    private int _state = 1;
+    public int state
+    {
+        get => _state;
+        set
+        {
+            //When state is set, it will automatically update the animator's state
+            myAnim.SetInteger("state", value);
+            _state = myAnim.GetInteger("state"); //keep _state accurate
+        }
+    }
+   
+    private Animator myAnim;
+
+    private void Start()
+    {
+        myAnim = GetComponent<Animator>();
+    }
+
+    void Update()
+    {
+       
+    }
+}
