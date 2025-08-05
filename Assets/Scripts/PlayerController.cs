0b6d65072855c75b9f5a16aae59528f29e21791a
jnielsen713 <mickeyman713@gmail.com>
2025-08-04 20:32:12 -0700

    Added Boosters

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -7,6 +7,12 @@ public class PlayerController : MonoBehaviour
     public bool playerCanMove;
     public float playerSpeed;
 
+    //Boost-related variables
+    public bool isBoosting;
+    private Vector3 boostDirection;
+    private float boostSpeed;
+    private float boostTimer;
+
     [SerializeField] private InputActionReference Move;
 
     private Vector3 inputMovement;
@@ -15,13 +21,20 @@ public class PlayerController : MonoBehaviour
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        
+
     }
 
     // Update is called once per frame
     void Update()
     {
-        if (playerCanMove) Movement();
+        if (isBoosting)
+        {
+            Boost();
+        }
+        else if (playerCanMove)
+        {
+            Movement();
+        }
     }
 
     void Movement()
@@ -34,7 +47,7 @@ public class PlayerController : MonoBehaviour
         inputMovement.Normalize();
 
         RaycastHit2D rayCheck = Physics2D.Raycast(transform.position, transform.TransformDirection(inputMovement), 0.33f);
-        if(rayCheck)
+        if (rayCheck)
         {
             if (rayCheck.transform.gameObject.layer == LayerMask.NameToLayer("Horizontal Wall"))
             {
@@ -48,7 +61,32 @@ public class PlayerController : MonoBehaviour
         }
 
 
-            transform.Translate(inputMovement * playerSpeed * Time.deltaTime);
+        transform.Translate(inputMovement * playerSpeed * Time.deltaTime);
+    }
+
+    //The alternative to Movement which is called while Boosting
+    void Boost()
+    {
+        //Loops until boost timer runs out
+        transform.Translate(boostDirection * boostSpeed * Time.deltaTime);
+        boostTimer -= Time.deltaTime;
+        if (boostTimer < 0)
+        {
+            isBoosting = false;
+        }
+    }
+
+    /*
+    Right now this will boost on any TriggerEnter,
+    so if we want to use that for projectiles etc we will need to modify.
+    */
+    void OnTriggerEnter2D(Collider2D booster)
+    {
+        //Default boost settings. Direction is where booster points (local up).
+        boostDirection = booster.transform.up.normalized;
+        boostSpeed = 10f;
+        boostTimer = 0.2f;
+        isBoosting = true;
     }
 
 }
